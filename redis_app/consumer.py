9b383534e66031585f34c9c988abf7fe5ebdbf1d
AlejandroColina <alejo_9102@hotmail.com>
2022-07-09 16:20:20 -0500

    for producer

--- redis_app/consumer.py
+++ redis_app/consumer.py
@@ -9,13 +9,15 @@ url_post = 'http://localhost:8000/metrics'
 def consumer():
     try:
         response = r.xread({'STREAM':b'0-0'})
-        if (len(response)):
+        if len(response):
             for line in response[0][1]:
                 res = requests.post(url_post, json=line[1])
                 if res.status_code == 201:
+                    print('Metric processed!')
                     r.xdel('STREAM', line[0].encode("ascii"))
+
         else:
-            print('No hay mensajes en redis para procesar.')
+            print('There are no messages in redis to process.')
     except ResponseError as e:
         print(e)
 
